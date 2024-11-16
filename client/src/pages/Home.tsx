87dc2d4f1650d07b83b9099929927fd5dde68f17
Victor Roman <Romantech91>
2024-11-15 20:23:44 -0700

    home.css file to adjust home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import MapDisplay from '../components/MapDisplay.tsx';
+import './Home.css';
 
 const Home: React.FC = () => {
   return (
-    <div className="home-container">
+    <div className="home-container container-fluid">
       {/* Title */}
-      <h1 className="title">Trek Mate</h1>
+      <h1 className="title text-center">Trek Mate</h1>
 
       {/* Search Bar */}
       <div className="search-bar-container">
-        <input type="text" placeholder="Search for a trail or campsite..." />
+        <input type="text" className='form-control' placeholder="Search for a trail or campsite..." />
       </div>
 
       {/* Main Content */}
-      <div className="main-content">
+      <div className="main-content row">
         {/* Sidebar for state list */}
-        <div className="sidebar">
-          <button>Nav Bar</button>
+        <div className="sidebar col-3">
+          <button className='btn btn-primary'>Nav Bar</button>
           <div>List by state</div>
         </div>
 
         {/* Map Area */}
-        <div className="map-container">
+        <div className="map-container col-9">
           {/* Placeholder for DisplayMap */}
           <MapDisplay />
         </div>
