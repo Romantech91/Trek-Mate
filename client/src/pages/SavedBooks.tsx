f35498210c512c27a00f64982f11f597f132135c
Victor Roman <Romantech91>
2024-11-16 20:53:20 -0700

    updated api.ts

--- client/src/pages/SavedBooks.tsx
+++ client/src/pages/SavedBooks.tsx
@@ -3,11 +3,11 @@ import { Container, Card, Button, Row, Col } from 'react-bootstrap';
 
 // import { deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
-//import { removeBookId } from '../utils/localStorage';
+import { removeBookId } from '../utils/localStorage';
 
 import { useQuery, useMutation } from '@apollo/client';
 import { GET_ME } from '../utils/queries';
-//import { REMOVE_BOOK } from '../utils/mutations';
+import { REMOVE_BOOK } from '../utils/mutations';
 import { User } from '../models/User';
 import { Book } from '../models/Place';
 
