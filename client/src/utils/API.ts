6e51521b9e39fbc6a36fd55348ea0beb45f3277c
Vtencouchclimbr <148500829+Vtencouchclimbr@users.noreply.github.com>
2024-11-18 20:08:34 -0700

    text errors

--- client/src/utils/API.ts
+++ client/src/utils/API.ts
@@ -1,18 +1,17 @@
 
-//@ts-ignore
-const API_KEY = '';
-// make a search to maps api
+// const API_KEY = 'SE2pqSQA5gDRSax0XSmb6keLsi0GWsbIaOAc1p56';
+// make a search to nps api
 export const searchNPS = async (query: string) => {
   try {
+    const url = `https://developer.nps.gov/api/v1/campgrounds?stateCode=${query}&api_key=${import.meta.env.VITE_NPS_API_KEY}`;
     const response = await fetch(
-      `https://developer.nps.gov/api/v1/campgrounds?stateCode=${query}&api_key=${API_KEY}`
+      url
     );
-
     if (!response.ok) {
       throw new Error('Failed to fetch data from the API.');
     }
-
-    return await response.json(); // Parse the JSON data here
+    const campgrounds = await response.json();
+    return campgrounds.data; // Parse the JSON data here
   } catch (error) {
     console.error(error);
     return null;
