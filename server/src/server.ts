06c83d3a837731bb82368d4a6d6a79bbe2b34666
Vtencouchclimbr <148500829+Vtencouchclimbr@users.noreply.github.com>
2024-11-24 14:33:24 -0700

    debug deploy

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'node:path';
 import type { Request, Response } from 'express';
 import db from './config/connection.js';
-import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/server-express
+import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './utils/auth.js';
@@ -10,7 +10,6 @@ import { fileURLToPath } from 'node:url';
 import { dirname } from 'node:path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-// import routes from './routes/index.js';
 
 const server = new ApolloServer({
   typeDefs,
